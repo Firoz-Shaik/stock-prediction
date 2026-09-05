47ebd312e1c8326767a5bff81241e42d406d48ba
Firoz-Shaik <fsfiroz115@gmail.com>
2026-09-05 18:03:19 +0530

    Integrated register api endpoint in the frontend

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
 import './assets/css/style.css'
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
-
+import Register from './components/Register'
+import Login from './components/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
---
+++ react-frontend/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='row justify-content-center'>
+            <div className='col-md-6'>
+                <h3 className='text-light text-center mb-4'>Log In</h3>
+            </div>
+        </div>
+    </div>  
+    </>
+  )
+}
+
+export default Login
