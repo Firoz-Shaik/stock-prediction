c06cf7b119d0c7dbd613260781370cc20fc01866
Firoz-Shaik <fsfiroz115@gmail.com>
2026-09-05 14:01:39 +0530

    Added Homepage

---
+++ react-frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='bg-dark text-light text-center p-3 mt-5'>
+        <hr className='border-bottom'/>
+        <p className='text-light text-center'>© 2024 Stock Predictor. All rights reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ react-frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded-3 mt-5'>
+          <h1 className='display-4 text-white'>Welcome to Stock Predictor</h1>
+          <p className='lead text-white'>Predict stock prices with our advanced machine learning model.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
